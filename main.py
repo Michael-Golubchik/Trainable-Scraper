346dbe3df062c872821965e1ac9d667177aad6b7
Michael-Golubchik <m_golubchik@mail.ru>
2023-09-15 14:38:36 +0300

    Some import.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # TrainingDatasetBuilder class for creating a training dataset from source data.
 # The source data includes pages and XPath elements to click on, in order to open the desired pages.
 
+from bs4 import BeautifulSoup
+from lxml import etree
+
 class TrainingDatasetBuilder:
+    # In order this class to work you need to create folder 'data' in the project folder root.
+    # And extract files and folders from page.zip there
+    # Also, it is necessary to add pages.csv to folder 'data'
+
     # Arguments:
     # page_dir - the directory where bank website pages are located
     # bank_i - bank index
